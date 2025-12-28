e1a82331722dd7f28de441af9fa24a089d2cd340
Freeman <4031216+fmnxl@users.noreply.github.com>
2025-12-28 18:26:12 +0700

    feat(nix): add NixOS modules for amc-radio and amc-bot

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     };
   };
 
-  outputs = inputs @ { flake-parts, ... }:
+  outputs = inputs @ { flake-parts, self, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
@@ -79,5 +79,77 @@
 
           packages.default = pythonSet.mkVirtualEnv "amc-peripheral-env" workspace.deps.default;
         };
+
+      flake = {
+        nixosModules.default = { config, lib, pkgs, ... }:
+          let
+            cfg = config.services.amc-peripheral;
+          in
+          {
+            options.services.amc-peripheral = {
+              enable = lib.mkEnableOption "AMC Peripheral Services";
+              environmentFile = lib.mkOption {
+                type = lib.types.path;
+                description = "Path to the environment file containing secrets.";
+              };
+              cookiesPath = lib.mkOption {
+                type = lib.types.path;
+                description = "Path to the cookies file.";
+              };
+            };
+
+            config = lib.mkIf cfg.enable {
+              systemd.services.amc-radio = {
+                wantedBy = [ "multi-user.target" ];
+                after = [ "network.target" "motortown-server.service" ];
+                description = "AMC Radio Service";
+                environment = {
+                  PLAYLIST_PATH = "/var/lib/radio/playlist";
+                  FFPROBE_PATH = "${pkgs.ffmpeg}/bin/ffprobe";
+                  DENO_PATH = "${pkgs.deno}/bin/deno";
+                  RADIO_PATH = "/var/lib/radio/";
+                  REQUESTS_PATH = "/var/lib/radio/requests";
+                  SONGS_PATH = "/var/lib/radio/songs";
+                  JINGLES_PATH = "/var/lib/radio/jingles";
+                  FFMPEG_PATH = "${pkgs.ffmpeg}/bin/ffmpeg";
+                  GOOGLE_APPLICATION_CREDENTIALS = "/var/lib/radio/adc.json";
+                  OPUS_PATH = "${pkgs.libopus}/lib/libopus.so";
+                  YT_COOKIES_PATH = "${cfg.cookiesPath}";
+                };
+                restartIfChanged = true;
+                serviceConfig = {
+                  Type = "simple";
+                  Restart = "on-failure";
+                  RestartSec = "10";
+                  RuntimeMaxSec = "26400";
+                  EnvironmentFile = "${cfg.environmentFile}";
+                };
+                script = ''
+                  ${self.packages.${pkgs.system}.default}/bin/amc_radio
+                '';
+              };
+
+              systemd.services.amc-bot = {
+                wantedBy = [ "multi-user.target" ];
+                after = [ "network.target" "motortown-server.service" ];
+                description = "AMC Bot";
+                environment = {
+                  FFMPEG_PATH = "${pkgs.ffmpeg}/bin/ffmpeg";
+                  FFPROBE_PATH = "${pkgs.ffmpeg}/bin/ffprobe";
+                };
+                restartIfChanged = true;
+                serviceConfig = {
+                  Type = "simple";
+                  Restart = "on-failure";
+                  RestartSec = "10";
+                  EnvironmentFile = "${cfg.environmentFile}";
+                };
+                script = ''
+                  ${self.packages.${pkgs.system}.default}/bin/amc_bot
+                '';
+              };
+            };
+          };
+      };
     };
 }
